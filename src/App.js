c9aa2347c3259a54535027053297fb452c30f5d4
Masterpet <eprenza@gmail.com>
2023-08-09 10:41:48 -0400

    se agregaron los tres componentes

--- src/App.js
+++ src/App.js
@@ -6,7 +6,30 @@ function App() {
     <div className="App">
       <div className='contenedor-principal'>
         <h1>Esto es lo que dicen nuestros estudiantes</h1>
-        <Testimonio/>
+        <Testimonio 
+          nombre='Shawn Wang'
+          pais='Singapur'
+          imagen='shawn'
+          cargo='Ingeniero de software'
+          empresa='Amazon'
+          testimonio='Hola, me encanta tu curso y como enseñas, se logra aprender muy bien. Quería además recomendarte que hicieras una actualización al uso de "create-react-app" ya que en la nueva documentación este ya no es recomendado por react y ha desaparecido de allí, dice que se debería de usar con algún framework pero creo que para aprender también es excelente'/>
+        
+        <Testimonio 
+          nombre='Sarah Chima'
+          pais='Nigeria'
+          imagen='sarah'
+          cargo='Ingeniero de software'
+          empresa='Chackdesk'
+          testimonio='Hola, me encanta tu curso y como enseñas, se logra aprender muy bien. Quería además recomendarte que hicieras una actualización al uso de "create-react-app" ya que en la nueva documentación este ya no es recomendado por react y ha desaparecido de allí, dice que se debería de usar con algún framework pero creo que para aprender también es excelente'/>
+        
+        <Testimonio 
+          nombre='Emma Bostian'
+          pais='Suecia'
+          imagen='emma'
+          cargo='Ingeniero de software'
+          empresa='Spotify'
+          testimonio='Hola, me encanta tu curso y como enseñas, se logra aprender muy bien. Quería además recomendarte que hicieras una actualización al uso de "create-react-app" ya que en la nueva documentación este ya no es recomendado por react y ha desaparecido de allí, dice que se debería de usar con algún framework pero creo que para aprender también es excelente'/>
+        
       </div>
     </div>
   );
--- src/componentes/testimonios.js
+++ src/componentes/testimonios.js
@@ -1,14 +1,14 @@
 import React from "react";
 import '../stylesheets/testimonio.css'
 
-export function Testimonio(){
+export function Testimonio(props){
     return(
         <div className="contenedor-testimonio">
-            <img className="imagen-testimonio" src={require("../imagenes/testimonio-emma.png")} alt="foto"/>
+            <img className="imagen-testimonio" src={require(`../imagenes/testimonio-${props.imagen}.png`)} alt="foto"/>
             <div className="contenedor-texto-testimonio">
-                <p className="nombre-testimonio">Emma Bostian en Suecia</p>
-                <p className="cargo-testimonio">ingeniera de software</p>
-                <p className="texto-testimonio">"Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod officia iure quis vero sint voluptatibus repudiandae nihil provident necessitatibus deserunt delectus aut inventore quaerat atque quibusdam culpa ab, doloremque rerum?"</p>
+                <p className="nombre-testimonio">{props.nombre} en {props.pais}</p>
+                <p className="cargo-testimonio">{props.cargo} en {props.empresa}</p>
+                <p className="texto-testimonio">"{props.testimonio}"</p>
             </div>
         </div>
     )
